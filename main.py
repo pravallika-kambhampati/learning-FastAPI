5b86b6a7be5d3bbc522edaef2fcbf0771a4fd938
Kambhampati Pravallika (ppravall) <ppravall@cisco.com>
2022-04-06 17:24:15 +0530

    Added about route

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Data": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/about")
+def about():
+    return {"Data":"About"}
